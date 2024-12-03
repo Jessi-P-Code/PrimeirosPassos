85214fd77828c5be343d0e802b6a9c64c6952c58
Jessi-P-Code <drikarvat@hotmail.com>
2024-12-03 14:16:17 -0300

    You're all dead tu mi

--- php/index.php
+++ php/index.php
@@ -1,5 +1,8 @@
 <?php
-
-
-
+    $nome = $_GET['nome'];
+    $telefone = $_GET['telefone'];
+    $email = $_GET['e-mail'];
+    $novidades = $_GET['novidades'];
+    $mensagem = $_GET['mensagem'];
+    echo('Meu nome é: ' . $nome  . '</br>' . 'Meu telefone é: ' . $telefone . '</br>' . 'Meu e-mail é: ' . $email . '</br>' . 'Aceito novidades: ' . $novidades . '</br>' . 'Minha mensagem: ' . $mensagem);
 ?>
